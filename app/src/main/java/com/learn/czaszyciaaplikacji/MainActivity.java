8387665db596109575bf846c4ad07c98dcae4467
Szymon1948 <szymonintrys@gmail.com>
2022-11-03 14:08:03 +0100

    Czas Zycia.3

--- app/src/main/java/com/learn/czaszyciaaplikacji/MainActivity.java
+++ app/src/main/java/com/learn/czaszyciaaplikacji/MainActivity.java
@@ -1,8 +1,10 @@
 package com.learn.czaszyciaaplikacji;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -14,6 +16,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         licznikTextView = findViewById(R.id.licznikTextView);
+        Log.i("CzasZycia","wywołana metoda onCreate");
+        if(savedInstanceState!=null){
+            licznik = savedInstanceState.getInt("Licznik",0);
+            licznikTextView.setText(licznik.toString());
+            Log.i("czas zycia","wywołana metoda onSaveInstanceState");
+        }
+
     }
     public void zwieksz(View view){
         licznik++;
@@ -22,5 +31,47 @@ public class MainActivity extends AppCompatActivity {
     public void zmiejsz(View view){
         licznik--;
         licznikTextView.setText(licznik.toString());
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+         outState.putInt("Licznik",licznik);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("CzasZycia","wywołana metoda onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("CzasZycia","wywołana metoda onStop");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("CzasZycia","wywołana metoda onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("CzasZycia","wywołana metoda onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("CzasZycia","wywołana metoda onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("CzasZycia","wywołana metoda onDestroy");
     }
 }
